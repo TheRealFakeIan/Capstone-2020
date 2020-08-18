cf1dc375282c14f096bb06a0a9043ebc3c5bf119
timleecy <63772274+timleecy@users.noreply.github.com>
2020-08-18 11:59:00 +1000

    Add files via upload

--- shape_colour.py
+++ shape_colour.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Aug  2 15:42:50 2020
+Created on Mon Aug 17 14:00:41 2020
 
 @author: timot
 """
@@ -12,26 +12,61 @@ import sys
 import json
 import project_modules as pm
 
-black  = (0,181,0,255,0,50)
-white  = (0,179,0,26,230,255)
-red    = (0,8,127,255,127,255)
-orange = (9,20,127,255,127,255)
-yellow = (21,30,127,255,127,255)
-green  = (31,85,127,255,127,255)
-blue   = (86,125,127,255,127,255)
-purple = (126,140,127,255,127,255)
-pink   = (141,175,127,255,127,255)
-brown  = (9,20,127,255,51,153)
+black  = (0,179,0,255,0,64)
+white  = (0,179,0,64,192,255)
+red1   = (0,8,64,255,64,255)
+red2   = (141,179,64,255,64,255) #includes pink
+orange = (9,20,64,255,64,255) #brown may overlap
+yellow = (21,30,64,255,64,255)
+green  = (31,85,64,255,64,255)
+blue   = (86,125,64,255,64,255)
+purple = (126,140,64,255,64,255)
+test = (0,179,0,255,0,255)
+
 
 root = 'C:/Capstone2020/ActiveVisionDataset/'
-
-#if len(sys.argv) > 1:
 scene_name = sys.argv[1]
 scene_path = os.path.join(root,scene_name)
 target_colour = sys.argv[2]
 target_shape = sys.argv[3]
 
+if target_colour == 'red':
+    lower_colour_1 = np.array([red1[0], red1[2], red1[4]])
+    upper_colour_1 = np.array([red1[1], red1[3], red1[5]])
+    lower_colour_2 = np.array([red2[0], red2[2], red2[4]])
+    upper_colour_2 = np.array([red2[1], red2[3], red2[5]])
+else:
+    colour = vars()[target_colour]
+    lower_colour = np.array([colour[0], colour[2], colour[4]])
+    upper_colour = np.array([colour[1], colour[3], colour[5]])
 
+
+'''
+print('Press \'Esc\' once you are done choosing colour range')
+def nothing(x):
+    pass
+cv.namedWindow('Trackbars')
+cv.createTrackbar('Lower Hue', 'Trackbars', 0, 179, nothing)
+cv.createTrackbar('Upper Hue', 'Trackbars', 0, 179, nothing)
+cv.createTrackbar('Lower Sat', 'Trackbars', 0, 255, nothing)
+cv.createTrackbar('Upper Sat', 'Trackbars', 0, 255, nothing)
+cv.createTrackbar('Lower Val', 'Trackbars', 0, 255, nothing)
+cv.createTrackbar('Upper Val', 'Trackbars', 0, 255, nothing)
+key = cv.waitKey(-1)
+
+if key == 27:
+    lower_hue = cv.getTrackbarPos('Lower Hue', 'Trackbars')
+    upper_hue = cv.getTrackbarPos('Upper Hue', 'Trackbars')
+    lower_sat = cv.getTrackbarPos('Lower Sat', 'Trackbars')
+    upper_sat = cv.getTrackbarPos('Upper Sat', 'Trackbars')
+    lower_val = cv.getTrackbarPos('Lower Val', 'Trackbars')
+    upper_val = cv.getTrackbarPos('Upper Val', 'Trackbars')
+    lower_colour = np.array([lower_hue, lower_sat, lower_val])
+    upper_colour = np.array([upper_hue, upper_sat, upper_val])
+    
+    cv.destroyAllWindows()'''
+    
+ 
 #set up scene specfic paths
 images_path = os.path.join(scene_path,'jpg_rgb')
 annotations_path = os.path.join(scene_path,'annotations.json')
@@ -47,76 +82,81 @@ cur_image_name = image_names[0]
 next_image_name = '' 
 move_command = ''
 
-colour = vars()[target_colour]
-lower_colour = np.array([colour[0], colour[2], colour[4]])
-upper_colour = np.array([colour[1], colour[3], colour[5]])
 
-
-key = 0
 while True:
 
     img = cv.imread(os.path.join(images_path,cur_image_name))
-    blur = cv.medianBlur(img,5)
-    #blur = cv.GaussianBlur(img, (3, 3), 0)
-    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    hsv_blur = cv.cvtColor(blur,cv.COLOR_BGR2HSV)
-        
-    mask = cv.inRange(hsv, lower_colour, upper_colour)
-    mask_blur = cv.inRange(hsv_blur, lower_colour, upper_colour)
-    kernel = np.ones((5,5), np.uint8)
-    mask = cv.erode(mask, kernel)
-    mask_blur = cv.erode(mask_blur, kernel)
-      
-    res = cv.bitwise_and(blur, blur, mask=mask_blur)
-    gray = cv.cvtColor(res, cv.COLOR_BGR2GRAY)
-    edges = cv.Canny(gray, 50, 250)
-    
-    #cv.imshow('mask', mask_blur)
-    
 
-    contours, hierarchy = cv.findContours(mask_blur ,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
-    #contours, hierarchy = cv.findContours(mask,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-    
-    biggest_cnt_area = 0
-    biggest_cnt = 0    
-    for cnt in contours: 
-        area = cv.contourArea(cnt) 
+    if target_shape == 'circle':
+        blur = cv.medianBlur(img, 3)
+        hsv = cv.cvtColor(blur, cv.COLOR_BGR2HSV)
         
-        if area > biggest_cnt_area:
-            biggest_cnt_area = area
-            biggest_cnt = cnt
-   
-    # Shortlisting the regions based on there area. 
-    if biggest_cnt_area > 2000:  
-        approx = cv.approxPolyDP(biggest_cnt,  
-                              0.05 * cv.arcLength(biggest_cnt, True), True) 
-        
-        
-        if target_shape == 'square' and len(approx) == 4:
-            x,y,w,h = cv.boundingRect(approx)
-            aspectRatio = float(w/h)
-            if aspectRatio >= 0.95 and aspectRatio <= 1.05:
-                cv.drawContours(img, [approx], 0, (255, 0, 0), 5)
-                
-        elif target_shape == 'rectangle' and len(approx) == 4:
-            x,y,w,h = cv.boundingRect(approx)
-            aspectRatio = float(w/h)
-            if aspectRatio <= 0.95 or aspectRatio >= 1.05:
-                cv.drawContours(img, [approx], 0, (255, 0, 0), 5)
-                
-        elif target_shape == 'triangle' and len(approx) == 3:
-            cv.drawContours(img, [approx], 0, (255, 0, 0), 5)
+        if target_colour == 'red':
+            mask1 = cv.inRange(hsv, lower_colour_1, upper_colour_1)
+            mask2 = cv.inRange(hsv, lower_colour_2, upper_colour_2)
+            mask = cv.bitwise_or(mask1, mask2)
+        else:
+            mask = cv.inRange(hsv, lower_colour, upper_colour)      
             
-        elif target_shape == 'circle': #and len(approx) >= 10:
-            circles = cv.HoughCircles(edges,cv.HOUGH_GRADIENT,1,500,
+        res = cv.bitwise_and(blur, blur, mask=mask)
+        gray = cv.cvtColor(res, cv.COLOR_BGR2GRAY)
+        edges = cv.Canny(gray, 50, 250)   
+        circles = cv.HoughCircles(edges,cv.HOUGH_GRADIENT,1,500,
                               param1=200,param2=100,minRadius=0,maxRadius=0)
-            if circles is not None:
-                circles = np.uint16(np.around(circles))
-                for i in circles[0,:]:
-                    
-                    cv.circle(img,(i[0],i[1]),i[2],(255,0,0),5)
+        cv.imshow('mask', edges)
+        
+        if circles is not None:
+            circles = np.uint16(np.around(circles))        
+            for i in circles[0,:]:      
+                area = np.pi*((i[2])**2)
                 
+                if area>3000:
+                    cv.circle(img,(i[0],i[1]),i[2],(0,0,255),5)
+                
+    else:
+        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        blur = cv.medianBlur(hsv, 7) #stick to 7
+        
+        if target_colour == 'red':
+            mask1 = cv.inRange(hsv, lower_colour_1, upper_colour_1)
+            mask2 = cv.inRange(hsv, lower_colour_2, upper_colour_2)
+            mask = cv.bitwise_or(mask1, mask2)
+        else:
+            mask = cv.inRange(hsv, lower_colour, upper_colour)   
+        
+        kernel = np.ones((5,5), np.uint8)
+        mask = cv.erode(mask, kernel)
+        edges = cv.Canny(mask, 50, 250)
+     
+        #cv.imshow('mask', mask)
+
+        contours, hierarchy = cv.findContours(edges ,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+    
+        for cnt in contours: 
+            area = cv.contourArea(cnt) 
             
+            # Shortlisting the regions based on there area. 
+            if area > 3000:  #stick to 3000 for now
+                approx = cv.approxPolyDP(cnt,  
+                                      0.05 * cv.arcLength(cnt, True), True) #dont change 0.05
+                
+                
+                if target_shape == 'square' and len(approx) == 4:
+                    x,y,w,h = cv.boundingRect(approx)
+                    aspectRatio = float(w/h)
+                    if aspectRatio >= 0.95 and aspectRatio <= 1.05:
+                        cv.drawContours(img, [approx], 0, (0, 0, 255), 5)
+                        
+                elif target_shape == 'rectangle' and len(approx) == 4:
+                    x,y,w,h = cv.boundingRect(approx)
+                    aspectRatio = float(w/h)
+                    if aspectRatio <= 0.95 or aspectRatio >= 1.05:
+                        cv.drawContours(img, [approx], 0, (0, 0, 255), 5)
+                        
+                elif target_shape == 'triangle' and len(approx) == 3:
+                    cv.drawContours(img, [approx], 0, (0, 0, 255), 5)
+                
+    
                
     cv.namedWindow('image', cv.WINDOW_NORMAL)
     cv.resizeWindow('image', 960, 540)
@@ -162,3 +202,4 @@ while True:
   
         
   
+
